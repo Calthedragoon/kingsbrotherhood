792a9d080e8573e2a13ed98b42632c9b24627b11
Calthedragoon <Calgilly40@gmail.com>
2017-11-02 18:41:13 -0400

    The first addition of the main files used in the project

---
+++ js/One home mobile script.js
@@ -0,0 +1,1 @@
+var curpatch =("The current patch avaliable for play is Patch 4.11. The Ultimate Coil of Bahamut raid has been added. Emergency maintenance has been performed in order to fix issues brought along at launch. There is a remaining HUD layout issue still in effect for some, please check the Lodestone for more details.");
