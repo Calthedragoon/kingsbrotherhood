ac65929f1fc681807fb1639558e034d276e44f24
Calthedragoon <Calgilly40@gmail.com>
2017-11-03 01:08:46 -0400

    Added JavaScript for the Slideshow and slight html and css changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+/*Javascript to Run Slideshow in Header*/
+var current = 0,
+    slides = document.getElementsByClassName("arr");
 
+setInterval(function() {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0;
+  }
+  current = (current != slides.length - 1) ? current + 1 : 0;
+  slides[current].style.opacity = 1;
+}, 3000);
