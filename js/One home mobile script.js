caf057f06897e184a1b75f64beabce490a899887
Calthedragoon <Calgilly40@gmail.com>
2017-11-02 21:24:43 -0400

    additional css for patch button and slight changes to fonts and html code

--- js/One home mobile script.js
+++ js/One home mobile script.js
@@ -1,1 +1,5 @@
-var curpatch =("The current patch avaliable for play is Patch 4.11. The Ultimate Coil of Bahamut raid has been added. Emergency maintenance has been performed in order to fix issues brought along at launch. There is a remaining HUD layout issue still in effect for some, please check the Lodestone for more details.");
+var patchinfo = ("The current Patch update is 4.11 with minor hotfixes. There is still a current lingering HUD layout issue for some users. The Ultimate Coil of Bahamut raid has been added!");
+
+function print(var patchinfo) {
+  document.write(var patchinfo);
+}
