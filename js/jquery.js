dc988f4555d2b763fb5e254178747bda265fbd0e
Calthedragoon <Calgilly40@gmail.com>
2017-11-06 05:40:46 -0500

    Beginning major improvements to CSS and Fonts

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,4 @@
-var patchinfo = ("The current Patch update is 4.11 with minor hotfixes. There is still a current lingering HUD layout issue for some users. The Ultimate Coil of Bahamut raid has been added!");
+var patchinfo = prompt('');
 
 $(document).ready(function() {
   $('button').click(function(event){
