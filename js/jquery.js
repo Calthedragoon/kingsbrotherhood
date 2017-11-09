5dfe25344f9bb69101fa68fd6c3dad1b73a6229c
Calthedragoon <Calgilly40@gmail.com>
2017-11-09 04:29:34 -0500

    Added toggle buttons for pictures, Slideshow updated

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,15 @@
-var patchinfo = ;
 $(document).ready(function() {
-  $('button').click(function(event){
-    alert(patchinfo);
+  $('.cardtext').hide();
+
+  $("#pictext1").click(function(){
+    $("#cardtext1").toggle();
+  });
+
+  $("#pictext2").click(function(){
+    $("#cardtext2").toggle();
+  });
+
+  $("#pictext3").click(function(){
+    $("#cardtext3").toggle();
   });
 });
--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 
+/*Slideshow Vanilla Script*/
+var slideindex = 0;
+showslides();
+
+function showslides() {
+  var i;
+  var slide = document.getElementsByClassName("slides fade");
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
+  }
+  slideindex++;
+  if (slideindex > slide.length) {slideindex = 1}
+    slide[slideindex-1].style.display = "block";
+    setTimeout(showslides, 4000);
+}
