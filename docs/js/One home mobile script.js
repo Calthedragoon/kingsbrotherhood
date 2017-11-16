748f2416f5d787d352af95f9a0580ba45b522cf1
Calthedragoon <Calgilly40@gmail.com>
2017-11-16 01:08:36 -0500

    moved all files to central location

--- docs/js/One home mobile script.js
+++
@@ -1,7 +0,0 @@
-var patchinfo = ("The current Patch update is 4.11 with minor hotfixes. There is still a current lingering HUD layout issue for some users. The Ultimate Coil of Bahamut raid has been added!");
-
-$(document).ready(function() {
-  $('#information').click(function)(event){
-    alert(patchinfo);
-  });
-});
